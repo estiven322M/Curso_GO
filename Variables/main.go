5f14555be8c9b1a71b6235456e091b4dfcfa62c0
estiven322M <jhons.mendeza@uqvirtual.edu.co>
2024-07-16 22:14:56 -0500

    primer commit

---
+++ Variables/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var dog, cat = "Waw Waw Waw soy un perro muy bravo", "Miau Miau soy un gato muy perezoso"
+
+	fmt.Println(dog, cat) //imprimir el valor
+
+}
